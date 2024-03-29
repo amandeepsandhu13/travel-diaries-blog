3318d210060172d7100363bc75a971cd98bb32b9
Amandeep <samandeepkaur34@gmail.com>
2024-03-28 23:57:14 -0400

    final code submitted

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,18 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    const blogPostsContainer = document.querySelector('#blogPosts');
+    const blogPostsContainer = document.querySelector('#postList');
 
     // Function to render blog posts
     function renderBlogPosts() {
         
         const savedData = JSON.parse(localStorage.getItem('blogPost')) || [];
-
+        
         if (savedData.length === 0) {
+            
             let postElement = document.createElement('article');
             postElement.innerHTML = `
                 <p>No Post Added</p>
             `; blogPostsContainer.append(postElement);
         }else{
+            
             savedData.forEach(post => {
                 const postElement = document.createElement('article');
                 postElement.innerHTML = `
